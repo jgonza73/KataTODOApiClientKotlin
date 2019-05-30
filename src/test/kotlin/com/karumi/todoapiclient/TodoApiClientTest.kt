1958977bfc53897142ac2c9222ec3c019b2c085a
Barry <JoseAntonio.GonzalezLopez@betvictor.com>
2019-05-30 20:20:12 +0200

    Test for return task by id api

--- src/test/kotlin/com/karumi/todoapiclient/TodoApiClientTest.kt
+++ src/test/kotlin/com/karumi/todoapiclient/TodoApiClientTest.kt
@@ -5,6 +5,7 @@ import org.junit.Before
 import org.junit.Test
 import todoapiclient.TodoApiClient
 import todoapiclient.dto.TaskDto
+import todoapiclient.exception.ItemNotFoundError
 import todoapiclient.exception.UnknownApiError
 
 class TodoApiClientTest : MockWebServerTest() {
@@ -64,11 +65,52 @@ class TodoApiClientTest : MockWebServerTest() {
         assertEquals(UnknownApiError(418), error)
     }
 
+    @Test
+    fun sendGetRequestWhenGettingTodoById() {
+        enqueueMockResponse(200, FILENAME_GET_TASK_BY_ID_RESPONSE)
+
+        apiClient.getTaskById(ANY_TASK_ID)
+
+        assertGetRequestSentTo("/todos/$ANY_TASK_ID")
+    }
+
+    @Test
+    fun parsesTaskProperlyGettingTaskById() {
+        enqueueMockResponse(200, FILENAME_GET_TASK_BY_ID_RESPONSE)
+
+        val task = apiClient.getTaskById(ANY_TASK_ID).right!!
+
+        assertTaskContainsExpectedValues(task)
+    }
+
+    @Test
+    fun throwsUnknownExceptionWhenTheStatusCodeReturnedIsServerError() {
+        enqueueMockResponse(500)
+
+        val error = apiClient.getTaskById(ANY_TASK_ID).left!!
+
+        assertEquals(UnknownApiError(500), error)
+    }
+
+    @Test
+    fun throwsItemNotFoundWhenTheStatusCodeReturnedIsNotFound() {
+        enqueueMockResponse(404)
+
+        val error = apiClient.getTaskById(ANY_TASK_ID).left!!
+
+        assertEquals(ItemNotFoundError, error)
+    }
+
     private fun assertTaskContainsExpectedValues(task: TaskDto?) {
         assertTrue(task != null)
         assertEquals(task?.id, "1")
         assertEquals(task?.userId, "1")
         assertEquals(task?.title, "delectus aut autem")
         assertFalse(task!!.isFinished)
+    }
+
+    companion object {
+        const val ANY_TASK_ID = "any"
+        const val FILENAME_GET_TASK_BY_ID_RESPONSE = "getTaskByIdResponse.json"
     }
 }
